1761431b9eb353fc17a90c86c8312da14aecd5dd
verysetiawan <kangphery@gmail.com>
2020-05-16 23:04:23 +0700

    Hello Flask

--- flask2.py
+++ flask2.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    nilai = 100
-    #perulangan
-    angka = [1,2,3,4,5,6,7,8,9,10]
-    return render_template("index.html", value=nilai, angka=angka)
+@app.route("/index")
+def index():
+    
+    return "Hallo Flask Python"
 
 if __name__ == "__main__":
     app.run (debug=True)
