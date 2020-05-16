128a931fe3a7d8c44bd8cc059d8697290e5152f0
verysetiawan <kangphery@gmail.com>
2020-05-16 23:22:18 +0700

    menambahkan template index.html

--- flask2.py
+++ flask2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     
-    return "Hallo Flask Python"
+    return render_template ("index.html")
 
 if __name__ == "__main__":
     app.run (debug=True)
