14fec28cbacd7768b63235125bb3a5cae3f06cd7
verysetiawan <kangphery@gmail.com>
2020-05-16 23:49:15 +0700

    tambah variabel nilaiku dan parsing ke index

--- flask2.py
+++ flask2.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    #menambahkan nilai/variabel 
     nilaiku = 100
-    #menambahkan looping
-    hari = ("senin","selasa","rabu","kamis","jumat","sabtu")
-    return render_template ("index.html", variabelbaru=nilaiku, variabelhari=hari)
+    return render_template ("index.html", variabelbaru=nilaiku)
 
 if __name__ == "__main__":
     app.run (debug=True)
