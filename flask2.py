1cb026564026408c65342c74dd801a0a624ac107
verysetiawan <kangphery@gmail.com>
2020-05-16 23:10:15 +0700

    tambah tag html pada hello

--- flask2.py
+++ flask2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     
-    return "Hallo Flask Python"
+    return "<h1>Hallo Flask Python</h1>"
 
 if __name__ == "__main__":
     app.run (debug=True)
