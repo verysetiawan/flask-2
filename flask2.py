2c4b5597cb1aa6610139b8eb01b2f4d00c5bf984
verysetiawan <kangphery@gmail.com>
2020-05-17 01:29:57 +0700

    menggabungkan konten antar file (extend)

--- flask2.py
+++ flask2.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#membuat route untuk halaman /index dengan nama fungsi (def) indexku
 @app.route("/index")
-def index():
+def indexku():
     #menambahkan nilai/variabel 
     nilaiku = 100
     #menambahkan looping
@@ -11,6 +12,22 @@ def index():
     #conditioning if else
     statushari = "minggu" #jika hari adalah minggu maka libur, selainya kerja
     return render_template ("index.html", var1=nilaiku, var2=hari, var3=statushari)
+
+#membuat route untuk halaman /about dengan nama fungsi (def) about
+@app.route("/about")
+def about():
+    return render_template ("about.html")
+
+#membuat route untuk halaman /contact dengan nama fungsi (def) contact
+@app.route("/contact")
+def contact():
+    return render_template ("contact.html")
+
+#membuat route untuk halaman /main dengan nama fungsi (def) mainlay
+@app.route("/main")
+def mainlay():
+    return render_template ("main_layout.html")
+
 
 if __name__ == "__main__":
     app.run (debug=True)
