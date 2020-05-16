4fc8ad00b126c0292a78971ccd7aeacf82d0941c
verysetiawan <kangphery@gmail.com>
2020-05-16 23:12:52 +0700

    menambahkan tag h1 dan h3 pada return

--- flask2.py
+++ flask2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     
-    return "<h1>Hallo Flask Python</h1>"
+    return "<h1>Hallo Flask Python</h1> <br> <h3>Selamat datang di website flask sederhana</h3>"
 
 if __name__ == "__main__":
     app.run (debug=True)
